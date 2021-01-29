c07b3b137ab01ea530a7bf93b875a294be146ea2
CodeJA91 <mirroredperspective@protonmail.com>
2021-01-29 14:13:20 -0800

    Changing into let allows the string entered as the value to be later parsed into Integer.

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,11 @@ $(document).ready(function() {
     
     const qCatToyInput = prompt('What is your favorite cat toy?');
     const qLikeDogsInput = confirm('Dogs are "OK"...Or maybe they should be cancelled?');
-    const qCatYearsInput = prompt('From 0 to 100, how old are you in cat years?');
+    let qCatYearsInput = prompt('From 0 to 100, how old are you in cat years?');
     const qIndoorOutdoorInput = confirm('You enjoy the smell of a wet douglas fur as you poop in great litterbox?');
     const qCatchDotInput = confirm('You finally caught the phase-shifting red dot!')
-    console.log(qCatToyInput);
-    console.log(qLikeDogsInput);
-    console.log(qCatYearsInput);
-    console.log(qIndoorOutdoorInput);
-    console.log(qCatchDotInput);
+
+    
     
 
   });
