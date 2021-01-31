93af5a5941b4654ec8ec9f5cf61780202ddd354a
CodeJA91 <mirroredperspective@protonmail.com>
2021-01-31 00:58:53 -0800

    Lines 38-52 serves as the final iteration of a failsave. It will give an answer if the user enters a string that cannot be parsed into an integer and will still give a result to the user. The commented out lines serve to break up the code for easier-to-read chunks. I also did some rephrasing here and there.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     
     alert('Hmm..Interesting');
     
-    // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------------------
     
     let qLikeDogsInput = confirm('As a fellow feline, do you think dogs are simply "OK" to be within eyeshot?...Or maybe you think the outrage mob on Whisker® ought to "Cancel" them?');
     
@@ -16,9 +16,10 @@ $(document).ready(function() {
       $(alert('Hmm...I would say your priorities are bit...off. This will definitely impact your answer.'));
     } 
     
-    // ----------------------------------------------------------------------
+    // -----------------------------------------------------------------------------------------------------------------------------
     
     let qCatBellyRubsInput = parseInt(prompt('From 1 to 3, exactly how many belly rubs are allowed until the "clawAndBite()" function is called?'));
+    
     if (qCatBellyRubsInput === 1) {
       $(alert('Indeed...although, still a bit much, do you not agree?'));
       $('#JavaScript').hide();
@@ -34,30 +35,42 @@ $(document).ready(function() {
       $('#JavaScript').show();
       $('#Ruby').hide();
       $('#Python').hide();
-    } else (qCatBellyRubsInput = String) 
-      let qCatBellyRubsString = (confirm('Good sir, since when did letters take the place of numerals? Do you like breaking the rules?'));
+    } else {
+      (qCatBellyRubsInput = String)
+      let qCatBellyRubsStringInput = confirm('Good sir, do you like breaking the rules?');
+      if (qCatBellyRubsStringInput === true) {
+        $(alert('"All in academic fun." as they say!'));
+        $('#JavaScript').hide();
+        $('#Ruby').show();
+        $('#Python').hide();
+      } else {
+        $(alert('Maybe you should reconsider...'));
+        $('#JavaScript').show();
+        $('#Ruby').hide();
+        $('#Python').hide();
+      }
+    }
     
-    
-    // ----------------------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------------------------
     
     let qIndoorOutdoorInput = confirm('You enjoy the smell of a wet douglas fir as you defecate out in the great open litterbox?');
+    
     if (qIndoorOutdoorInput === true) {
       $(alert('I see you are a cat with highly developed tastes.'));
     } else {
       $(alert(' Well I for one find it almost intoxicatedly splendid'));
     }
     
-    // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------------------------
     
-    let qCatchDotInput = confirm('You frequently try and catch the phase-shifting red photon! Is it okay to assume this is what you do in your extracurricular activities?');
+    let qCatchDotInput = confirm('"You frequently try and catch the phase-shifting red photon!" Is it okay to assume this is what you do in your extracurricular activities?');
     
     if (qCatchDotInput === true) {
-      $(alert('I see you are a cat with highly developed tastes.'));
+      $(alert('A scholar of focus and dedication!'));
     } else {
-      $(alert('Well I for one find it intoxicatedly splendid!'));
+      $(alert('We all had to learn how to walk before we could pounce...keep at it!'));
     }
     
-    
-    alert('All Done! Check Below!');
+    alert('Gadzooks! That was rather...enlightening. My assessment is finished! Your answer lies below...');
   });
 });
